5195b52cb57df4151e56b81541a2cd9c9f789f4e
ticktak <mkersey@email.uscupstate.edu>
2017-09-07 20:43:59 -0400

    Modified command line arguments

--- src/SecretImage.java
+++ src/SecretImage.java
@@ -1,3 +1,14 @@
+/*
+ * Written by Matthew Kersey.
+ * 
+ * Current flags should be:
+ * -a - add
+ * -r - read
+ * -h - help... more to come later
+ * 
+ * 
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,22 +18,10 @@ import java.io.IOException;
 public class SecretImage {
 	
 	public static void main(String args[]) throws Exception{
-		if(args[0].equals("add")) {
-			StringBuilder sb = new StringBuilder();
-			for(int i=2; i<args.length; i++) {
-				sb.append(args[i]);
-			}
-			String dir = args[1];
-			String comment = sb.toString();
-			addComment(dir,comment);
-		}
-		else if(args[0].equals("read")){
-			readComments(args[1]);
-		}
-		
+		parseInput(args);
 	}
 	
-	public static void addComment(String dir, String comment) throws IOException {
+	public static void addComment(String dir, String comment[]) throws IOException {
 		File file = new File(dir);
 		long fileSize = file.length();
 		int size = (int) fileSize;
@@ -30,7 +29,17 @@ public class SecretImage {
 		FileInputStream fis = new FileInputStream(file);
 		fis.read(data);
 		JPEG jpeg = Parser.createJPEG(data);
-		jpeg.addComment(comment);
+//		StringBuilder sb = new StringBuilder(); 
+		String com = null;
+		for(int i=0; i<comment.length; i++) {
+//			sb.append(comment[i]);
+//			sb.append(" ");
+			System.out.println(com += comment[i] + " ");
+			com += comment[i] + " ";
+		}
+//		String newcomment = sb.toString();
+//		newcomment = newcomment.replace('!', ' ');
+		jpeg.addComment(com);
 		jpeg.writeFile(dir);
 	}
 
@@ -44,5 +53,33 @@ public class SecretImage {
 		JPEG jpeg = Parser.createJPEG(data);
 		jpeg.readComments();
 	}
+	
+	public static void removeComment() {
+		
+	}
+	
+	public static void help() {
+		
+	}
+	
+	public static void parseInput(String[] args) throws IOException {
+		//current flags, a, r, h
+		switch(args[0]) {
+		
+		case "-a": 
+			String [] comment = new String[args.length-2];
+			
+			for(int i=0; i<args.length-2; i++) {
+				comment[i] = args[i+2];
+			}
+			addComment(args[1], comment); break;
+		case "-r":
+			readComments(args[1]);
+			break;
+			
+		default: System.out.println("invalid flag");break;
+		}
+	}
 }
+ 
  
